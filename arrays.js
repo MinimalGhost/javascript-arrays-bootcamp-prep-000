44d8661fc6cd5b898b2f37c78b453bbb84426bc4
Eric Kollegger <eric.kollegger@gmail.com>
2017-10-20 14:18:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -30,11 +30,15 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array = array.slice(1);
+  array.slice(1);
   return array
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
   array.pop();
   return array
+}
+
+function removeElementFromEndOfArray(array) {
+  array.sl
 }
