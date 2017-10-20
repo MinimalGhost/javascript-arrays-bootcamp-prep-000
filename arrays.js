5740eaf07a493cc175b24a4edda4d0f0b9fa846f
Eric Kollegger <eric.kollegger@gmail.com>
2017-10-20 14:18:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -40,5 +40,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 function removeElementFromEndOfArray(array) {
-  array.sl
+  array = array.slice(0, array.length - 1);
+  return array
 }
